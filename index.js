aac60d94af491bfa9bff1cc2d1a7bff7f7184380
geetika rathore <110020464+geetika-rathore@users.noreply.github.com>
2023-05-08 18:40:03 +0530

    Add files via upload
    add more code

---
+++ index.js
@@ -0,0 +1,14 @@
+// about section
+const tab=document.querySelector(".about-tabs");
+const about_section=document.querySelector(".about-section");
+
+tab.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("tab-item") &&  !e.target.classList.contains("active")){
+tab.querySelector(".active").classList.remove("active");
+e.target.classList.add("active");
+
+const content=e.target.getAttribute("data-target");
+ans=about_section.querySelector(".tab-content.active").classList.remove("active");
+about_section.querySelector(content).classList.add("active");
+}
+});
